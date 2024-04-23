586a153ee2632af93fcb28fa2adb5d532eb411d6
zguo9908 <zguo9908@brandeis.edu>
2024-04-23 10:17:18 -0400

    trained two networks with ROC stats printed

--- multilayer-neural-network.py
+++ multilayer-neural-network.py
@@ -1,18 +1,19 @@
 import numpy as np
 from enum import Enum
-
-
-# Perceptron Class, used to create a perceptron object within a neural network
+import pandas as pd
 
 class Perceptron:
-    def __init__(self, weights: np.ndarray, bias: float):
+    def __init__(self, weights: np.ndarray, bias: float, is_categorical: bool, threshold):
         self.weights = weights
         self.bias = bias
         self.delta_weights = np.zeros_like(weights)
         self.delta_bias = 0
         self.activity = 0
         self.activation = 0
+        self.output = None
         self.delta = 0
+        self.is_categorical = is_categorical
+        self.output_threshold = threshold
 
     # Calculate the sigmoid activation function
     def calc_sig_activation(self):
@@ -20,6 +21,8 @@ class Perceptron:
 
     # Calculate the activity of the perceptron
     def calc_activity(self, inputs):
+        print(inputs)
+        print(self.weights)
         self.activity = np.dot(inputs, self.weights) + self.bias
 
     def calc_hidden_delta(self, next_layer_delta, next_layer_weights):
@@ -39,18 +42,29 @@ class Perceptron:
         self.weights += self.delta_weights
         self.bias += self.delta_bias
 
+    def train(self, inputs, target, eta, use_bias):
+        self.calc_activity(inputs)
+        self.calc_sig_activation()
+        error = target - self.activation
+        self.calc_output_delta(error)
+        self.set_delta_weights(inputs, eta, use_bias)
+        self.update_weights()
+
     def predict(self, inputs):
         self.calc_activity(inputs)
         self.calc_sig_activation()
-        print(f"Inputs: {inputs}")
+        if self.is_categorical:
+            self.output = 1 if self.activation > self.output_threshold else 0
+        else:
+            self.output = self.activation
 
+        print(f"Inputs: {inputs}")
         print(f"Activity Value: {self.activity}")
-        print(f"Activation Value: {self.activation}")
+        print(f"Activation Value: {self.output} because the data is {self.is_categorical} categorical")
 
 
 class NeuralNetwork:
     def __init__(self, hidden_layer_weights, output_layer_weights, bias, learning_rate, use_bias=False):
-        self.input_values = None
         self.input_values = None
         self.target = None
         self.hidden_layer_weights = hidden_layer_weights
@@ -63,9 +77,9 @@ class NeuralNetwork:
         self.big_error = None
 
         # Initialize the perceptron layers of the neural network
-        self.hidden_perceptron_1 = Perceptron(self.hidden_layer_weights[0], self.bias)
-        self.hidden_perceptron_2 = Perceptron(self.hidden_layer_weights[1], self.bias)
-        self.output_perceptron = Perceptron(self.output_layer_weights[0], self.bias)
+        self.hidden_perceptron_1 = Perceptron(self.hidden_layer_weights[0], self.bias, False, None)
+        self.hidden_perceptron_2 = Perceptron(self.hidden_layer_weights[1], self.bias, False, None)
+        self.output_perceptron = Perceptron(self.output_layer_weights[0], self.bias, True, 0)
 
     def feed_forward(self):
         self.hidden_perceptron_1.predict(self.input_values)
@@ -116,69 +130,100 @@ class NeuralNetwork:
 
 def main():
     # Define the weights and bias for the neural network
-    hidden_layer_weights = [np.array([0.3, 0.3]), np.array([0.3, 0.3])]
-    output_layer_weights = [np.array([0.8, 0.8])]
+    hidden_layer_weights = [np.random.rand(2), np.random.rand(2)]
+    output_layer_weights = [np.random.rand(2)]
     bias = 0
     learning_rate = 1
     use_bias = True
+    data = pd.read_csv('D:\\NeuralNetworkProject\\data.csv')
+    print(data)
 
-    class Method(Enum):
-        Test = 0
-        Method1 = 1
-        Method2 = 2
+    #--------------------TRAIN WITH SINGLE PERCEPTRON---------------------------#
+    weights = np.random.rand(2)
+    bias = -0.5
+    is_categorical = True
+    threshold = 0
 
-    method = Method.Method2.value
+    perceptron = Perceptron(weights, bias, is_categorical, threshold)
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for i in range(30):
+        print(f"Iteration {i + 1} --------------------------------------------------")
 
-    input_output_pair = [
-        #
-        (np.array([1, 1]), 0.9),
-        (np.array([-1, -1]), 0.05)
-    ]
+        for j in range(len(data)):
+            if j % 2 != 0:
+                inputs = data.iloc[j, 1:3].values
+                target = data.iloc[j, 3]
+                print(f"Input/Output Pair {j}: {inputs}, {target}")
+                perceptron.train(inputs, target, learning_rate, 1)
+                if perceptron.output == target:
+                    if target == 0:
+                        tn += 1
+                    else:
+                        tp += 1
+                else:
+                    if target == 1:
+                        fn += 1
+                    else:
+                        fp += 1
 
-    # Create a neural network with the defined weights and bias
-    nn = NeuralNetwork(hidden_layer_weights, output_layer_weights, bias, learning_rate, use_bias)
 
-    if method == 0:
-        # Test
-        print(f"Test --------------------------------------------------")
-        input_output_pair = [
-            (np.array([1, 2]), 0.7)
-        ]
-        nn.use_bias = False
-        for i in range(2):
-            print(f"Iteration {i + 1} --------------------------------------------------")
-            for iop_index, (inputs, target) in enumerate(input_output_pair):
-                print(
-                    f"Input/Output Pair {iop_index + 1}: {inputs}, {target} ==========================================")
-                nn.train(inputs, target)
+    print(f'weights after training {perceptron.weights}')
+    print(f'tn {tn}; tp {tp}; fn {fn}; fp {fp}')
+    if (fn+tn) > 0:
+        print(f'specifity of current network is {tn/(fn+tn)}')
+    if (fp+tp) > 0:
+        print(f'sensitivity of current network is {tp/(fp+tp)}')
+    if (tp+tn) > 0:
+        print(f'positive predictive value is {tp/(tp+tn)}')
+    if (fp+fn) > 0:
+        print(f'negative predictive value is {fn/(fn+fp)}')
 
-    elif method == 1:
-        # Method 1
-        print(f"Method 1 --------------------------------------------------")
-        for i in range(15):
-            print(f"Iteration {i + 1} --------------------------------------------------")
-            for iop_index, (inputs, target) in enumerate(input_output_pair):
-                print(
-                    f"Input/Output Pair {iop_index + 1}: {inputs}, {target} ==========================================")
-                nn.train(inputs, target)
-        for inputs, target in input_output_pair:
-            print(f"Final Output --------------------------------------------------")
-            print(f"Input: {inputs}, Target: {target}")
-            nn.target = target
-            nn.predict(inputs)
 
-    elif method == 2:
-        # Method 2
-        print(f"Method 2 --------------------------------------------------")
-        for inputs, target in input_output_pair:
-            for i in range(15):
-                print(f"Iteration {i + 1} --------------------------------------------------")
-                nn.train(inputs, target)
-        for inputs, target in input_output_pair:
-            print(f"Final Output --------------------------------------------------")
-            print(f"Input: {inputs}, Target: {target}")
-            nn.target = target
-            nn.predict(inputs)
+    #--------------------TRAIN WITH HIDDEN LAYER---------------------------#
+    nn_2 = NeuralNetwork(hidden_layer_weights, output_layer_weights, bias, learning_rate, use_bias)
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    print(f"Online training for odd numbered items --------------------------------------------------")
+    for i in range(30):
+        print(f"Iteration {i + 1} --------------------------------------------------")
+        for j in range(len(data)+1):
+            if j % 2 != 0:
+                inputs = data.iloc[j, 1:3].values
+                target = data.iloc[j, 3]
+                print(f"Input/Output Pair {j}: {inputs}, {target} ==========================================")
+                nn_2.train(inputs, target)
+                if nn_2.output_perceptron.output == target:
+                    if target == 0:
+                        tn += 1
+                    else:
+                        tp += 1
+                else:
+                    if target == 1:
+                        fn += 1
+                    else:
+                        fp += 1
+
+
+    print(f'tn {tn}; tp {tp}; fn {fn}; fp {fp}')
+    if (fn+tn) > 0:
+        print(f'specifity of current network is {tn/(fn+tn)}')
+    if (fp+tp) > 0:
+        print(f'sensitivity of current network is {tp/(fp+tp)}')
+    if (tp+tn) > 0:
+        print(f'positive predictive value is {tp/(tp+tn)}')
+    if (fp+fn) > 0:
+        print(f'negative predictive value is {fn/(fn+fp)}')
+
+    print(f'hidden layer weights after training: {nn_2.hidden_layer_weights}')
+    print(f'outer layer weights after training: {nn_2.output_layer_weights}')
+
+    # Determine threshold
+
 
 
 if __name__ == "__main__":
